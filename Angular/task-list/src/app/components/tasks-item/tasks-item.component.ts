cba0eae30fa22aca93f2f772f0dfb3e956eba79d
MartinBinaghi <martin.binaghi01@gmail.com>
2022-03-12 16:45:50 -0300

    delete terminado

--- Angular/task-list/src/app/components/tasks-item/tasks-item.component.ts
+++ Angular/task-list/src/app/components/tasks-item/tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import {Task} from '../../Task';
 import {TASKS} from '../../mock-tasks';
@@ -12,6 +12,7 @@ import {faTimes} from '@fortawesome/free-solid-svg-icons';
 export class TasksItemComponent implements OnInit {
 
   @Input() task: Task = TASKS [0];
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
   faTimes = faTimes;
 
   constructor() { }
@@ -19,8 +20,8 @@ export class TasksItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onDelete(){
-    console.log("Delete");
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
   }
 
 }
