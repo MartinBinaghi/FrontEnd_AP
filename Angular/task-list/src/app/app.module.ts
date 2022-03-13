0bfae98114c689817de2e322ad1ddd829a3f3828
MartinBinaghi <martin.binaghi01@gmail.com>
2022-03-13 18:25:33 -0300

    componente footer creado

--- Angular/task-list/src/app/app.module.ts
+++ Angular/task-list/src/app/app.module.ts
@@ -12,9 +12,11 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const appRoutes:Routes = [
-  { path: '', component:TasksComponent }
+  { path: '', component:TasksComponent },
+  { path: 'about', component:AboutComponent }
 ]
 
 @NgModule({
@@ -25,7 +27,8 @@ const appRoutes:Routes = [
     TasksComponent,
     TasksItemComponent,
     AddTaskComponent,
-    AboutComponent
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
