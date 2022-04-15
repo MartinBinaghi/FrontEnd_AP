e45c2bb7dfa952d61a89ab0ad7c9d7a6c0cf435a
MartinBinaghi <martin.binaghi01@gmail.com>
2022-04-15 19:11:54 -0300

    Experiencie component inyectado

--- Portfolio/PortfolioWeb/src/app/app.module.ts
+++ Portfolio/PortfolioWeb/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ThemeButtonComponent } from './components/theme-button/theme-button.component';
 import { HomeComponent } from './components/home/home.component';
 import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { EducationComponent } from './components/education/education.component';
     ThemeButtonComponent,
     HomeComponent,
     EducationComponent,
+    ExperienceComponent,
   ],
   imports: [
     BrowserModule
