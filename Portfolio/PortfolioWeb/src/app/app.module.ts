daac9539c25ea9b517fe45d958646a5ae1c56ab3
MartinBinaghi <martin.binaghi01@gmail.com>
2022-04-15 17:04:00 -0300

    Componente theme-button inyectado en header

--- Portfolio/PortfolioWeb/src/app/app.module.ts
+++ Portfolio/PortfolioWeb/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ThemeButtonComponent } from './components/theme-button/theme-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ThemeButtonComponent
   ],
   imports: [
     BrowserModule
