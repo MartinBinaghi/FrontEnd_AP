4dd46620a0dd4307641739b94e84e865f0b76e41
MartinBinaghi <martin.binaghi01@gmail.com>
2022-05-03 19:45:12 -0300

    commit prueba

--- Web estática/example01.ts
+++
@@ -1,5 +0,0 @@
-let a: number; //Inferencia explicita
-let b: string; //Inferencia explicita
-let c=101; //Inferencia implicita
-//c="one"
-let recursos: string[]=['memoria', 'disco', 'procesador']
--- Web estática/output/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let a; //Inferencia explicita
-let b; //Inferencia explicita
-let c = 101; //Inferencia implicita
-//c="one"
-let recursos = ['memoria', 'disco', 'procesador'];
-let message = 'Hello World!';
-console.log(message);
