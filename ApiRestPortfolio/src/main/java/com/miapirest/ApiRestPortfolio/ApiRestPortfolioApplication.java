258637b993affc4b837fc82a673f895a439350ee
MartinBinaghi <martin.binaghi01@gmail.com>
2022-06-05 18:45:20 -0300

    ApiRest added

---
+++ ApiRestPortfolio/src/main/java/com/miapirest/ApiRestPortfolio/ApiRestPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.miapirest.ApiRestPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestPortfolioApplication.class, args);
+	}
+
+}
---
+++ ApiRestPortfolio/src/test/java/com/miapirest/ApiRestPortfolio/ApiRestPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miapirest.ApiRestPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
