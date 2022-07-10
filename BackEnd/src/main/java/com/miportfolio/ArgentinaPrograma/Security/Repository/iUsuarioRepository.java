3929cdfa35cd89d1039ec5f85d6c3fda202e8e7e
MartinBinaghi <martin.binaghi01@gmail.com>
2022-07-10 18:26:38 -0300

    iUsuarioRepository.java update

---
+++ BackEnd/src/main/java/com/miportfolio/ArgentinaPrograma/Security/Repository/iUsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.miportfolio.ArgentinaPrograma.Security.Repository;
+
+import com.miportfolio.ArgentinaPrograma.Security.Entity.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iUsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+
+    boolean existsByNombreUsuario(String nombreUsuario);
+    boolean existsByEmail(String email);
+}
