09d169be051c31d8cdb8d00d74e4a9f3bf18b409
MartinBinaghi <martin.binaghi01@gmail.com>
2022-07-18 19:18:24 -0300

    error login Solved

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,6 +10,7 @@ import { TokenService } from 'src/app/services/token.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
   
   isLogged = false;
@@ -23,30 +24,28 @@ export class LoginComponent implements OnInit {
   constructor(private tokenService: TokenService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
-    if (this.tokenService.getToken()) {
+    if(this.tokenService.getToken()){
       this.isLogged = true;
       this.isLogginFail = false;
       this.roles = this.tokenService.getAuthorities();
     }
   }
 
-  onLogin(): void {
+  onLogin(): void{
     this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password); 
-    this.authService.login(this.loginUsuario).subscribe(
-      data => {
-        this.isLogged = true;
-        this.isLogginFail = false;
-        this.tokenService.setToken(data.token);
-        this.tokenService.setUsername(data.nombreUsuario);
-        this.tokenService.setAuthorities(data.authorities);
-        this.roles = data.authorities;
-        this.router.navigate(['']);
-      },
-      err => {
+    this.authService.login(this.loginUsuario).subscribe(data =>{
+      this.isLogged = true;
+      this.isLogginFail = false;
+      this.tokenService.setToken(data.token);
+      this.tokenService.setUserName(data.nombreUsuario);
+      this.tokenService.setAuthorities(data.authorities);
+      this.roles = data.authorities;
+      this.router.navigate([''])
+      }, err =>{
         this.isLogged = false;
         this.isLogginFail = true;
         this.errMsj = err.error.mensaje;
         console.log(this.errMsj);
-      });
-    }
+      })
+  }
 }
