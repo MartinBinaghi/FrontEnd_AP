c76f40b58e9e13e243dc0bae01d69e80f5794ecb
MartinBinaghi <martin.binaghi01@gmail.com>
2022-07-21 18:29:36 -0300

    update

---
+++ BackEnd/src/main/java/com/miportfolio/ArgentinaPrograma/Repository/RExperiencia.java
@@ -0,0 +1,12 @@
+package com.miportfolio.ArgentinaPrograma.Repository;
+
+import com.miportfolio.ArgentinaPrograma.Entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RExperiencia extends JpaRepository<Experiencia, Integer>{
+    public Optional<Experiencia> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
